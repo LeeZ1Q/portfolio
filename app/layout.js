8bcdfb925f490f98574fa742a2e450e5397eece6
LeeZ1Q <466944567@qq.com>
2023-06-13 17:24:19 +0800

    fix:lang="en"

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import NavBar from "./components/NavBar";
-import "./styles/global.css";
-import "./styles/markdown.css";
-import { Providers } from "./theme-provider";
-import { IBM_Plex_Sans, Caveat } from "next/font/google";
+import NavBar from './components/NavBar';
+import './styles/global.css';
+import './styles/markdown.css';
+import { Providers } from './theme-provider';
+import { IBM_Plex_Sans, Caveat } from 'next/font/google';
 
 const ibm = IBM_Plex_Sans({
-	subsets: ["latin"],
-	weight: "400",
-	variable: "--font-ibm",
+	subsets: ['latin'],
+	weight: '400',
+	variable: '--font-ibm',
 });
 
 const cav = Caveat({
-	subsets: ["latin"],
-	variable: "--font-cav",
+	subsets: ['latin'],
+	variable: '--font-cav',
 });
 
 export default function RootLayout({ children }) {
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
 		<html
 			className={`${ibm.variable} ${cav.variable}`}
 			suppressHydrationWarning={true}
+			lang='en'
 		>
 			<body>
 				<Providers>
